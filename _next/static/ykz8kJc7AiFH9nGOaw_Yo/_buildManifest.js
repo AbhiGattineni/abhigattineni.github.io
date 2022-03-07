b34f9f4a72ea7568690ac502cda33775625137aa
AbhiGattineni <AbhiGattineni@users.noreply.github.com>
2022-03-07 07:50:41 +0000

    deploy: c7b80118e00ad3dc6082d7939a014e2363629060

---
+++ _next/static/ykz8kJc7AiFH9nGOaw_Yo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b57d99ad93473ce45108.css","static/chunks/pages/index-97a6d2b9da707bb329bb.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
