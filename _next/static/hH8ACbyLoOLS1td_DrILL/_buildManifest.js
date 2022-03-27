a27c695f5a69a616233e81c5fd6a4fe5769a7adc
AbhiGattineni <AbhiGattineni@users.noreply.github.com>
2022-03-27 04:42:28 +0000

    deploy: 107197ef5ba47d2490c831872925decc6ae382ce

--- _next/static/hH8ACbyLoOLS1td_DrILL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b57d99ad93473ce45108.css","static/chunks/pages/index-3a0a71adcd0b3411e9b7.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
